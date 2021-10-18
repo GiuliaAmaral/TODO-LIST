c078e0a8344209b71316b143bcb413aea6b7fa52
Giulia Amaral <75877954+GiuliaAmaral@users.noreply.github.com>
2021-10-18 20:14:47 -0300

    Atualizando funções

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 // Criando uma função para criar os itens da lista
 
 let bancoDados = [
-    {"tarefa": "Estudar", "status":""},
-    {"tarefa": "netflix", "status":"checked"},
-    {"tarefa": "comer pipoca", "status":"checked"}
-    
+    { "tarefa": "Estudar", "status": "" },
+    { "tarefa": "netflix", "status": "checked" },
+    { "tarefa": "comer pipoca", "status": "checked" }
+
 
 ];
 
-let criarItem = (descricao,status="") => {
+function criarItem(descricao, status = "") {
     let item = document.createElement("label"); // nesta linha estou direcionando o código para criar um elemento do tipo label no documento HTML
     item.classList.add("todo__item"); // adicionando a classe todo-item ao label
     item.innerHTML = `
@@ -22,6 +22,21 @@ let criarItem = (descricao,status="") => {
     document.getElementById("todoList").appendChild(item); //Aqui estou pegando o elemento do html por ID e estou adicionando o elemento item a ele
 }
 
+// let criarItem = (descricao, status = "") => {
+//     let item = document.createElement("label"); // nesta linha estou direcionando o código para criar um elemento do tipo label no documento HTML
+//     item.classList.add("todo__item"); // adicionando a classe todo-item ao label
+//     item.innerHTML = `
+//         <input type="checkbox" ${status}>
+//         <div>${descricao}</div>
+//         <input type="button" value="X">
+    
+//     `
+//     document.getElementById("todoList").appendChild(item); //Aqui estou pegando o elemento do html por ID e estou adicionando o elemento item a ele
+
+// }
+
+
+
 let limparItem = () => {
     let toDoList = document.getElementById("todoList");
     while (toDoList.firstChild) {
@@ -31,9 +46,13 @@ let limparItem = () => {
 
 let atualizaLista = () => {
     limparItem();
-    bancoDados.forEach (item => criarItem (item.tarefa, item.status)); //adicionando as tarefas e status ao banco de dados sempre que for criado um novo item
+    bancoDados.forEach(item => criarItem(item.tarefa, item.status)); //adicionando as tarefas e status ao banco de dados sempre que for criado um novo item
 }
 
-atualizaLista();
+let inserirItem = () => {
 
-document.getElementById("nemItem")
+}
+
+document.getElementById("newItem").addEventListener("keypress", inserirItem);
+
+atualizaLista();
